ee0fa496af1ebd14d1c77c2385b0ad9d48ba822c
Shivam Goyal <shivamgoyal1506@outlook.com>
2019-07-10 12:18:42 +0530

    Added variables

---
+++ src/app/variables.prod.ts
@@ -0,0 +1,33 @@
+// Production
+const FB_APPID = '452524931986646';
+const SERVER_DOMAIN = 'https://caportal.ecelliitk.org/api/';
+
+// Local
+// const FB_APPID = '425044981657116';
+// const SERVER_DOMAIN = 'http://localhost:5200/';
+
+const FB_API_VERSION = 'v3.3';
+const SERVER_AUTH_URI = 'auth/';
+const SERVER_OAUTH_URI = 'Oauth/';
+const SERVER_DASHBOARD_URI = 'data/datasum';
+const SERVER_DATACOUNT_URI = 'data/datacount';
+const SERVER_POSTS_URI = 'data/posts';
+const SERVER_TASKS_URI = 'data/tasks';
+const SERVER_POSTSHARE_URI = 'data/postgain';
+
+// Session update interval (in seconds)
+const SESSION_UPDATE_INTERVAL = 1800;
+
+export {
+    FB_APPID,
+    FB_API_VERSION,
+    SERVER_DOMAIN,
+    SERVER_AUTH_URI,
+    SERVER_OAUTH_URI,
+    SERVER_DASHBOARD_URI,
+    SERVER_DATACOUNT_URI,
+    SERVER_POSTS_URI,
+    SERVER_TASKS_URI,
+    SERVER_POSTSHARE_URI,
+    SESSION_UPDATE_INTERVAL
+};
